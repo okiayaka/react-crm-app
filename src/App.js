81064e008b63b8320e97947c3fcdbe4999331774
大木綺香 <krcklcp10nknk@gmail.com>
2020-06-06 10:04:11 +0900

    値の取得

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
 
       // addDetas.push(detas)
 
-      props.setDetas(addDetas)
+      // props.setDetas(addDetas)
     }
     return (
       <>
@@ -48,13 +48,13 @@ const App = () => {
     // const userDeta = () => {
     // }
 
-    // // ＋ボタンが押された時以下の処理を実行
-    //   const incBtn = () => { 
-    //     // 1.props.detas[index].ageの数を取得
-    //     const newAge = props.detas[index].age.value
-    //     // 2.取得した値に＋１する。
-    //     newAge ++
-    //   }
+    // ＋ボタンが押された時以下の処理を実行
+      // const incBtn = () => { 
+      //   // 1.props.detas[index].ageの数を取得
+      //   const newAge = props.detas[index].age.value
+      //   // 2.取得した値に＋１する。
+      //   newAge ++
+      // }
 
     // カウントボタン
     // 1.年齢のvalueを取得 <- 値の取得がうまくいっていない
@@ -65,10 +65,36 @@ const App = () => {
       //   props.setDetas(newAge++)
       // }
 
-      const incBtn = (props) => { 
-        const incAge = props.detas[this.index].age.value
-        props.setDetas(incAge++)
+
+      // const incBtnにpropsは渡せない。（コンポーネント関数ではないから。）
+      // const incBtn = (props) => { 
+      //   const incAge = props.detas[this.index].age.value
+      //   props.setDetas(incAge++)
+      // }
+
+
+      // ----------- 0602-------------
+      // // reactでのクリックイベントの渡し方 ----- onClick = { 関数名 }
+      const incBtn = () => {
+        // 1. 年齢を取得
+        // const newAge = props.detas.age.value
+        // const newAge = detas.age.value
+        // const newAge = detas[this.index].age.value
+        // 2. +1 する
+        // newAge ++
+
+        // ---------06-04
+        // this.setState({value : this.state.value - 1});
+
+        // this.setDeta({value : this.deta.age.value - 1});
+
+        const newAge = this.deta.age.value
+        newAge++
+
+
       }
+
+      // const [count , setCount] = useState(0);
     
 
       const decBtn = (props) => { 
@@ -110,6 +136,7 @@ const App = () => {
           // 既存の配列.map(処理) => 既存の配列に処理をして返してくれる。
           // ？？.map((deta, index)？？
           props.detas.map((deta, index) => {
+            // detaをindex(ここでは[0]と[1])分、新しい配列にして生成。-----という意味？
             return (
               <>
               <ul>
@@ -121,6 +148,9 @@ const App = () => {
                 {/* {props.detas[index].age}の数を取得→1ずつ減らす、増やす処理（関数）。 */}
                 {/* <button onClick={decBtn}> - </button>
                 <button onClick={incBtn}> + </button> */}
+
+                {/* <button onClick={() => setCount(count + 1)}> - </button> */}
+
 
                 <button onClick={decBtn}> - </button>
                 <button onClick={incBtn}> + </button>
@@ -154,7 +184,7 @@ const App = () => {
             <button> + </button>
 
           <li>メール : {props.detas[1].email}</li>
-        </ul> */}
+        </ul>  */}
 
 
       </>
@@ -170,6 +200,7 @@ const App = () => {
       {/* 入力されたデータを反映 */}
       {/* <List></List> */}
       <List detas={detas} setDetas={setDetas}></List>
+      {/* <List detas={detas} index={index} setDetas={setDetas}></List> */}
 
     </section>
   );
